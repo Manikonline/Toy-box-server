7b9abb6356e86962b7aaaf10cb0e391b8bb2eb28
manik1241 <thmanik1241@gmail.com>
2023-05-21 18:51:21 +0600

    edit some data in versel.json and index.js

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 
-app.use(cors())
+
+const corsConfig={
+  origin:'*',
+  credential:true,
+  methods:['GET','POST', 'PUT','DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
+// app.use(cors())
 app.use(express.json())
 
 
@@ -25,7 +33,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollection= client.db('toyboxDB').collection('products');
 
@@ -111,10 +119,10 @@ async function run() {
 
 
     // create index
-    const indexKeys={toyName:1};
-    const indexOptions={name:'toyName'}
+    // const indexKeys={toyName:1};
+    // const indexOptions={name:'toyName'}
 
-    const result = await productCollection.createIndex(indexKeys,indexOptions)
+    // const result = await productCollection.createIndex(indexKeys,indexOptions)
 
     app.get('/toySearchBytoyName/:text',async(req,res)=>{
       const searchToy=req.params.text;
@@ -128,7 +136,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
